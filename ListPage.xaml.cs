2512c99b7a7b2e4a030fd23054417d69ff3aa719
Denisa <Denisa@DESKTOP-21R9RUQ>
2022-12-06 13:50:42 +0200

    lab9 cu erori

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,32 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    public Task<int> SaveListProductAsync(ListProduct listp)
+    {
+        if (listp.ID != 0)
+        {
+            return _database.UpdateAsync(listp);
+        }
+        else
+        {
+            return _database.InsertAsync(listp);
+        }
+    }
+    public Task<List<Product>> GetListProductsAsync(int shoplistid)
+    {
+        return _database.QueryAsync<Product>(
+        "select P.ID, P.Description from Product P"
+        + " inner join ListProduct LP"
+        + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+        shoplistid);
+    }
 }
